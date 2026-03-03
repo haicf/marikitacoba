0d313652d69f2e61bc24a7941ac4cc744e3135f9
Cheryn <a1145501@mail.nuk.edu.tw>
2026-03-03 08:45:56 +0000

    done until testcase

--- Problem/decoding(3/3/26).cpp
+++ Problem/decoding(3/3/26).cpp
@@ -2,11 +2,15 @@
 
 int main (){
     int T;
-    scanf(%d,&T);
+    scanf("%d",&T);
 
-    for (t =1; t<T; t++);
-    scanf(%s, &t);
+    for (int t =1; t<T; t++){
+        char s [1005];
+        scanf ("%s", &s);
 
+        printf ("Case %d : \n", t);
+    }
+    
 
 
     return 0;
